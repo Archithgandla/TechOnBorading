8b1654e41d716be35f80b313baafd1b79e187ed1
ArchithGandla <Archithgandla1010@gmail.com>
2023-08-19 23:08:32 +0530

    initial changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/one.jpeg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "First Image";
 
 /**
  * Project list
@@ -31,28 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Quiz App",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Qui App using react",
+    url: "https://github.com/Archithgandla/React-Quiz-App",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Authentication API using Express JS",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Created a authentication API using express js and mongoDB.",
+    url: "https://github.com/Archithgandla/Authentication_API",
   },
   {
-    title: "My Resume Site",
+    title: "Leetcode solutions",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Written code for some of the most important DSA problems on leetcode.",
+    url: "https://github.com/Archithgandla/Leetcode-Solutions",
   },
 ];
 
